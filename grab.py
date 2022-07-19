a5c1188d27c048d44f203142ae4e8bb6d347640d
andrewrs <881737+andrewrs@users.noreply.github.com>
2022-07-19 13:57:35 -0400

    Create grab.py

---
+++ grab.py
@@ -0,0 +1,50 @@
+# import necessary libraries
+from bs4 import BeautifulSoup
+import requests
+import re
+  
+  
+# function to extract html document from given url
+def getHTMLdocument(url):
+      
+    # request for HTML document of given url
+    response = requests.get(url)
+      
+    # response will be provided in JSON format
+    return response.text
+  
+
+
+
+i = 2
+while i < 5:
+  print(i)
+
+  # assign required credentials
+  # assign URL
+
+  urlbegin = "https://aspace.gmu.edu/repositories/resources?page="
+
+  url_to_scrape = urlbegin + str(i) 
+  
+  # create document
+  html_document = getHTMLdocument(url_to_scrape)
+  
+  # create soap object
+  soup = BeautifulSoup(html_document, 'html.parser')
+  
+  
+  # find all the anchor tags with "href" 
+  # attribute starting with "https://"
+  for link in soup.find_all('a', 
+                          attrs={'href': re.compile("^/resources")}):
+    # display the actual urls
+    print(link.get('href'))  
+
+
+  i += 1
+
+
+
+
+    
